8c0fdb26d8e615fdf5acc5130fcecb305d925285
TechLounge777 <msblanche777@gmail.com>
2020-10-12 15:08:53 +0200

    add vue router

--- vuex-ecommerce-store/src/main.js
+++ vuex-ecommerce-store/src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from './App.vue';
+
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: routes
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  router, 
   render: h => h(App),
 }).$mount('#app')
