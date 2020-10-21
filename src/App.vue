136024e4ebb28dfcbabeffda529e61749972bdaf
TechLounge777 <msblanche777@gmail.com>
2020-10-21 19:44:45 +0200

    added top nav with vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+      <v-toolbar dark>
+          <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-toolbar-title>ShoppingCart</v-toolbar-title>
+    
+      <v-spacer></v-spacer>
+
+      <v-toolbar-item>
+        <v-btn flat>
+          Products
+        </v-btn>
+        <v-btn flat>
+          Basket
+        </v-btn>
+      </v-toolbar-item>
+    </v-toolbar>
+
+
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
